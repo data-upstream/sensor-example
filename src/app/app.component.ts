808407ae4008a3e00bdc210973be60cabd36511f
Alex <info@sp33c.de>
2018-02-21 11:05:49 +0100

    latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  data: Object;
+
+  baseUri = 'https://db.alpha.data-upstream.ch/api';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  makeRequest(): void {
+    const options = {
+      params: {
+        device_ids: ['0b2bed85-92f5-40d5-a9d7-3886db2964cb']
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        // READ ONLY TOKEN
+        'X-Access-Token': '3442bee0-0d02-4db4-b5e5-066de46931ab'
+      }
+    };
+    this.http.get(this.baseUri + '/aggregate_log_data', options).subscribe(
+      data => {
+        this.data = data;
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.makeRequest();
+  }
+
+
 }
