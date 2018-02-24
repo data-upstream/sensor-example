d4f9285e56f3973f4229cea81495dfc031ac9a2b
Alex <info@sp33c.de>
2018-02-24 16:27:26 +0100

    implemented dummy graph jqxchart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import * as moment from 'moment';
-import { DecimalPipe } from '@angular/common';
+import { jqxChartComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxchart';
 
 /**
  * interface LogData
@@ -37,6 +37,55 @@ class TimeSeriesItem {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  @ViewChild('myChart') myChart: jqxChartComponent;
+
+  sampleData: any[] = [29, undefined, 10, 15, 10, undefined, NaN, 30, 25, undefined, 33, 19, 11];
+
+  padding: any = { left: 5, top: 5, right: 15, bottom: 5 };
+
+  titlePadding: any = { left: 0, top: 0, right: 0, bottom: 10 };
+
+  xAxis: any =
+    {
+      text: 'x',
+      valuesOnTicks: false
+    };
+
+  seriesGroups: any[] =
+    [
+      {
+        type: 'line',
+        source: this.sampleData,
+        toolTipFormatFunction: (value: any, itemIndex: any, serie: any, group: any, categoryValue: any, categoryAxis: any) => {
+          let dataItem = this.sampleData[itemIndex];
+          return '<DIV style="text-align:left"><b>Index:</b> ' +
+            itemIndex + '<br /><b>Value:</b> ' +
+            value + '<br /></DIV>';
+        },
+        valueAxis:
+          {
+            title: { text: 'Value<br>' }
+          },
+        series:
+          [
+            { emptyPointsDisplay: 'skip', displayText: 'Value', lineWidth: 2, symbolSize: 8, symbolType: 'circle' }
+          ]
+      }
+    ];
+
+  dropDownOnSelect(event: any): void {
+    let chartInstance = this.myChart.getInstance();
+    let args = event.args;
+
+    if (args) {
+      let value = args.item.value;
+      chartInstance.seriesGroups[0].series[0].emptyPointsDisplay = value;
+      chartInstance.update();
+    }
+
+  }
+
   title = 'Sensor Report - Biel/Bienne (CH)';
 
   last_updated: Date = null;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LOCALE_ID } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { jqxChartComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxchart';
+import { jqxDropDownListComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxdropdownlist';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, jqxChartComponent, jqxDropDownListComponent
   ],
   imports: [
     BrowserModule,
@@ -22,3 +23,4 @@ export class AppModule { }
 platformBrowserDynamic().bootstrapModule(AppModule, {
   providers: [{provide: LOCALE_ID, useValue: 'de-DE' }]
 });
+
